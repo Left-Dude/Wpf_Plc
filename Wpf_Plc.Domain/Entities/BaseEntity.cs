f9fc33fa4caa3fefc91ba1d112fcedf65223091c
Constantine Strelets <strelets7953@gmail.com>
2025-07-08 14:17:10 +0300

    Добавил Entity для PLC

---
+++ Wpf_Plc.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Wpf_Plc.Domain.Entities;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+    public DateTime? Created { get; set; } = DateTime.UtcNow;
+    public DateTime? Modified { get; set; } = DateTime.UtcNow;
+}
---
+++ Wpf_Plc.Domain/Entities/PLCDevice.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.IO.Ports;
+using Wpf_Plc.Domain.Enums;
+
+namespace Wpf_Plc.Domain.Entities;
+
+public class PLCDevice : BaseEntity
+{
+    public PLCModel Model { get; set; }
+    
+    // Для алгоритма
+    public IPAddress IPAddress { get; set; }
+    public SerialPort SerialPort { get; set; }
+}
