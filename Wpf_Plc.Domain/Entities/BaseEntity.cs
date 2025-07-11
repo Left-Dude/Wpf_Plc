24be13812a77db9aea31590a6631aba6911eef77
Constantine Strelets <strelets7953@gmail.com>
2025-07-11 12:18:44 +0300

    Добавил сущность для программы

--- Wpf_Plc.Domain/Entities/BaseEntity.cs
+++ Wpf_Plc.Domain/Entities/BaseEntity.cs
@@ -2,7 +2,7 @@
 
 public abstract class BaseEntity
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
     public DateTime? Created { get; set; } = DateTime.UtcNow;
     public DateTime? Modified { get; set; } = DateTime.UtcNow;
 }
---
+++ Wpf_Plc.Domain/Entities/PLCProgram.cs
@@ -0,0 +1,12 @@
+﻿namespace Wpf_Plc.Domain.Entities;
+
+public class PLCProgram : BaseEntity
+{
+    public string Name { get; set; }
+    public string OriginalFileName { get; set; }
+    public long SizeBytes { get; set; }
+
+    //Связи
+    public int PLCDeviceId { get; set; }
+    public PLCDevice PLCDevice { get; set; }
+}
